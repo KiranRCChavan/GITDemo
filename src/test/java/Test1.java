b1fe4103dec39c5e9b3550f19e377c91a0a6d741
KiranRCChavan <40853284+KiranRCChavan@users.noreply.github.com>
2023-03-11 19:24:26 -0500

    Update Test1.java

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -3,6 +3,5 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello");
          System.out.println("hell1");
-         System.out.println("hello2");
     }
 }
